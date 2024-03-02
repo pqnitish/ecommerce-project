c76ba7061e9ab583fd77b468a968347e981f1b06
nitish <pqnitish2020@gmail.com>
2024-03-02 21:07:10 +0530

    react-project

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Nav from './Nav';
+
+const Header = () => {
+  return <MainHeader>
+    <Link to ="/">
+        <img src='https://www.kohls.com/' alt='logo'className='logo'/>
+
+    </Link>
+    <Nav/>
+  </MainHeader>;
+};
+const MainHeader = styled.header`
+paddings: 0 4.8rem;
+height: 8rem;
+background-color:#617785;
+bacground-color: ${({theme})=>theme.colors.bg};
+display: flex;
+justify-content: space-around;
+align-items: center;
+position: relative;
+.logo{
+  position:absolute;
+  display:block;
+    height: 7rem;
+    top:6px;
+    left:6px;
+}
+`;
+
+export default Header
